ad862b0c4ba44faeda4de967a52d4eabda435bbb
Alex <cigan@ua.fm>
2021-04-30 16:09:43 +0300

    SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,38 @@
 import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
 import {PopapComponent} from './popap/popap.component';
 import {RefDirective} from './popap/ref.directive';
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 
 
 export class AppComponent {
 
-  @ViewChild(RefDirective, {static: false}) refDir: RefDirective;
+    @ViewChild(RefDirective, {static: false}) refDir: RefDirective;
 
-  constructor(private resolver: ComponentFactoryResolver) {}
+    constructor(
+        private resolver: ComponentFactoryResolver,
+        private title: Title,
+        private meta: Meta
+    ) {
+        this.title.setTitle('New title');
+        this.meta.addTags([
+            {name: 'keywords', content: 'angular, IT, JS'},
+            {name: 'description', content: 'this is app about angular'}
+        ]);
+    }
 
-  showModal() {
-    const modalFactory = this.resolver.resolveComponentFactory(PopapComponent);
-    const component = this.refDir.containerRef.createComponent(modalFactory);
-    component.instance.title = 'Dynamic title';
-    component.instance.close.subscribe(() => {
-      this.refDir.containerRef.clear();
-    });
-  }
+    showModal() {
+        const modalFactory = this.resolver.resolveComponentFactory(PopapComponent);
+        const component = this.refDir.containerRef.createComponent(modalFactory);
+        component.instance.title = 'Dynamic title';
+        component.instance.close.subscribe(() => {
+            this.refDir.containerRef.clear();
+        });
+    }
 }
 
