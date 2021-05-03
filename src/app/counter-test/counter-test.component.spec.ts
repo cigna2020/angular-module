8cdb631e381867eb7cc9ba45de65b870b70e4c74
Alex <cigan@ua.fm>
2021-05-03 13:49:46 +0300

    Integration testing, testing template, check if the counter from DOM equal a number from the test

--- src/app/counter-test/counter-test.component.spec.ts
+++ src/app/counter-test/counter-test.component.spec.ts
@@ -1,5 +1,6 @@
 import {CounterTestComponent} from './counter-test.component';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from "@angular/platform-browser";
 
 describe('CounterComponent', () => {
     let component: CounterTestComponent;
@@ -12,7 +13,15 @@ describe('CounterComponent', () => {
         fixture = TestBed.createComponent(CounterTestComponent);
         component = fixture.componentInstance;
     });
-    it('should be created the component',  () => {
+    it('should be created the component', () => {
         expect(component).toBeDefined();
+    });
+    it('should render counter property', () => {
+        const num = 42;
+        component.counter = num;
+        fixture.detectChanges(); // сообщаем Angular о том, что произошли изменения
+        const debElement = fixture.debugElement.query(By.css('.counter'));
+        const elem: HTMLElement = debElement.nativeElement;
+        expect(elem.textContent).toContain(num.toString());
     });
 });
